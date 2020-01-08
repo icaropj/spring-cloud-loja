ffccd2635d7f2d79e861fdeeb9e36dd118691a56
Icaro Pinho Esmeraldo <icaro.edeploy@stelo.com.br>
2020-01-08 16:40:54 -0300

    Swagger on fornecedor api

--- fornecedor/src/main/java/br/com/alura/microservice/fornecedor/FornecedorApplication.java
+++ fornecedor/src/main/java/br/com/alura/microservice/fornecedor/FornecedorApplication.java
@@ -3,9 +3,11 @@ package br.com.alura.microservice.fornecedor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableResourceServer
+@EnableSwagger2
 public class FornecedorApplication {
 
 	public static void main(String[] args) {
