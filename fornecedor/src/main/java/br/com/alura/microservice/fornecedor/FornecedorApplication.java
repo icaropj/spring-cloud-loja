3efecb68d3f5a52fd5065fb3676a3f99371a3e56
Icaro Pinho Esmeraldo <icaro.edeploy@stelo.com.br>
2020-01-08 16:47:49 -0300

    Enable cache

--- fornecedor/src/main/java/br/com/alura/microservice/fornecedor/FornecedorApplication.java
+++ fornecedor/src/main/java/br/com/alura/microservice/fornecedor/FornecedorApplication.java
@@ -2,12 +2,14 @@ package br.com.alura.microservice.fornecedor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableResourceServer
 @EnableSwagger2
+@EnableCaching
 public class FornecedorApplication {
 
 	public static void main(String[] args) {
--- fornecedor/src/main/java/br/com/alura/microservice/fornecedor/service/ProdutoService.java
+++ fornecedor/src/main/java/br/com/alura/microservice/fornecedor/service/ProdutoService.java
@@ -3,17 +3,20 @@ package br.com.alura.microservice.fornecedor.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import br.com.alura.microservice.fornecedor.model.Produto;
 import br.com.alura.microservice.fornecedor.repository.ProdutoRepository;
+
 
 @Service
 public class ProdutoService {
 
 	@Autowired
 	private ProdutoRepository produtoRepository;
-	
+
+	@Cacheable(value = "produtosPorEstado")
 	public List<Produto> getProdutosPorEstado(String estado) {
 		return produtoRepository.findByEstado(estado);
 	}
